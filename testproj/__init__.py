de8070f2f55b4f6e0efba6aac92abf013d5e283d
Max Mu <msmu0103@gmail.com>
2022-06-09 16:52:30 -0400

    File changes

--- testproj/__init__.py
+++ testproj/__init__.py
@@ -7,15 +7,12 @@ app = Flask(__name__)
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = '9AYNdyF4xS$D6V'
 app.config['MYSQL_DATABASE_DB'] = 'ksl-data'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_HOST'] = 'sqluser'
 mysql = MySQL(app)
 
-'''conn = mysql.connect()
-cursor = conn.cursor()'''
-
 try:
-    connection = connector.connect(host='127.0.0.1', database='ksl-data',
-         user='root', password='9AYNdyF4xS$D6V')
+    connection = connector.connect(host='localhost', database='ksl-data',
+         user='sqluser', password='9AYNdyF4xS$D6V')
     if connection.is_connected():
         db_Info = connection.get_server_info()
         print("Connected to MySQL Server version ", db_Info)
@@ -24,4 +21,4 @@ try:
         record = cursor.fetchone()
         print("You're connected to database: ", record)
 except Error as e:
-    print("Error while connecting to MySQL", e)
+    print("Error while connecting to MySQL, Error", e)
